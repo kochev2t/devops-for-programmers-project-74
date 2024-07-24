528001c3e56474d8324266864f1501c735c91636
kochev_2t <132632275+kochev2t@users.noreply.github.com>
2024-07-24 06:46:14 +0000

    Update config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   development: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
   },
   production: {
     dialect: 'postgres',
@@ -15,6 +16,12 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   // test: {
   //   dialect: 'sqlite',
